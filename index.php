738999729ba8fc76bd993a2aeaef56d525443f22
AngelYordan <dementiumx01@gmail.com>
2026-03-21 21:15:16 -0500

    Simplify summary traffic columns with dynamic units

--- index.php
+++ index.php
@@ -120,20 +120,20 @@
         return sprintf("%0.2f KiB", ($bytes/1024));
     }
 
-    function kbytes_to_mb_string($kb)
+    function kbytes_to_dynamic_string($kb)
     {
         $bytes = $kb * 1024;
-        $megabytes = $bytes / (1000 * 1000);
+        $units = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
+        $unit_index = 0;
+        $value = $bytes;
 
-        return number_format($megabytes, 2, '.', ',')." MB";
-    }
+        while ($value >= 1000 && $unit_index < count($units) - 1)
+        {
+            $value = $value / 1000;
+            $unit_index++;
+        }
 
-    function kbytes_to_gb_string($kb)
-    {
-        $bytes = $kb * 1024;
-        $gigabytes = $bytes / (1000 * 1000 * 1000);
-
-        return number_format($gigabytes, 2, '.', '')." GB";
+        return number_format($value, 2, '.', ',')." ".$units[$unit_index];
     }
 
     function write_summary()
@@ -182,18 +182,10 @@
         print "<table width=\"100%\" cellspacing=\"0\">\n";
         print "<caption>$caption</caption>\n";
         print "<tr>";
-        print "<th class=\"label\" style=\"width:120px;\" rowspan=\"2\">&nbsp;</th>";
-        print "<th class=\"label\" colspan=\"2\">".T('In')."</th>";
-        print "<th class=\"label\" colspan=\"2\">".T('Out')."</th>";
-        print "<th class=\"label\" colspan=\"2\">".T('Total')."</th>";
-        print "</tr>\n";
-        print "<tr>";
-        print "<th class=\"label\">MB</th>";
-        print "<th class=\"label gb_col\">GB</th>";
-        print "<th class=\"label\">MB</th>";
-        print "<th class=\"label gb_col\">GB</th>";
-        print "<th class=\"label\">MB</th>";
-        print "<th class=\"label gb_col\">GB</th>";
+        print "<th class=\"label\" style=\"width:120px;\">&nbsp;</th>";
+        print "<th class=\"label\">".T('In')."</th>";
+        print "<th class=\"label\">".T('Out')."</th>";
+        print "<th class=\"label\">".T('Total')."</th>";
         print "</tr>\n";
 
         for ($i=0; $i<count($tab); $i++)
@@ -201,21 +193,15 @@
             if ($tab[$i]['act'] == 1)
             {
                 $t = $tab[$i]['label'];
-                $rx_mb = kbytes_to_mb_string($tab[$i]['rx']);
-                $rx_gb = kbytes_to_gb_string($tab[$i]['rx']);
-                $tx_mb = kbytes_to_mb_string($tab[$i]['tx']);
-                $tx_gb = kbytes_to_gb_string($tab[$i]['tx']);
-                $total_mb = kbytes_to_mb_string($tab[$i]['rx']+$tab[$i]['tx']);
-                $total_gb = kbytes_to_gb_string($tab[$i]['rx']+$tab[$i]['tx']);
+                $rx_value = kbytes_to_dynamic_string($tab[$i]['rx']);
+                $tx_value = kbytes_to_dynamic_string($tab[$i]['tx']);
+                $total_value = kbytes_to_dynamic_string($tab[$i]['rx']+$tab[$i]['tx']);
                 $id = ($i & 1) ? 'odd' : 'even';
                 print "<tr>";
                 print "<td class=\"label_$id\">$t</td>";
-                print "<td class=\"numeric_$id\">$rx_mb</td>";
-                print "<td class=\"numeric_$id gb_col\">$rx_gb</td>";
-                print "<td class=\"numeric_$id\">$tx_mb</td>";
-                print "<td class=\"numeric_$id gb_col\">$tx_gb</td>";
-                print "<td class=\"numeric_$id\">$total_mb</td>";
-                print "<td class=\"numeric_$id gb_col\">$total_gb</td>";
+                print "<td class=\"numeric_$id\">$rx_value</td>";
+                print "<td class=\"numeric_$id\">$tx_value</td>";
+                print "<td class=\"numeric_$id\">$total_value</td>";
                 print "</tr>\n";
              }
         }
